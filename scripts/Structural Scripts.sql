c6cb149e97c7c62bb4c43f38b227cdc2107a2cc9
Hammad Latif <47081452+hmd-ltf@users.noreply.github.com>
2023-07-07 15:56:42 -0700

    added another table

--- scripts/Structural Scripts.sql
+++ scripts/Structural Scripts.sql
@@ -1,6 +1,13 @@
-CREATE TABLE IF NOT EXISTS users
-(
+CREATE TABLE IF NOT EXISTS users (
     id       SERIAL PRIMARY KEY,
-    username VARCHAR(32) UNIQUE NOT NULL,
-    password TEXT               NOT NULL
-);
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email    VARCHAR(255) UNIQUE,
+    password VARCHAR(255)        NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS organizations (
+    id      SERIAL PRIMARY KEY,
+    name    VARCHAR(255) UNIQUE NOT NULL,
+    address TEXT UNIQUE,
+    email   VARCHAR(255) UNIQUE
+    );
