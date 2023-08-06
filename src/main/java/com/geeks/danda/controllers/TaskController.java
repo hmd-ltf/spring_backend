acc7e390d7dc47d0382694e1bf98a6e313acf642
hmd <mhmd.ltf786@gmail.com>
2023-08-06 18:14:31 +0500

    small changes

--- src/main/java/com/geeks/danda/controllers/TaskController.java
+++ src/main/java/com/geeks/danda/controllers/TaskController.java
@@ -18,10 +18,10 @@ public class TaskController {
 
     private final TaskService taskService;
 
+    @Autowired
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
-
 
     @GetMapping
     public List<TaskDetails> findAllTasks() {
